ee5ecdda5dfa50b71538eaaaa0f4905dfbb26895
Radu <flo.radu_88@yahoo.com>
2015-10-01 21:24:23 +0100

    added setup for web application

--- DGraph.Config/Program.cs
+++ DGraph.Config/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace DGraph.Config
+﻿using IisConfiguration;
+using IisConfiguration.Logging;
+using Microsoft.Web.Administration;
+using System;
+
+namespace DGraph.Core.Configuration
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+            var logger = new ConsoleLogger();
+            var serverConfig = new WebServerConfig(logger);
+
+            if (!serverConfig.IsIis7OrAbove)
+            {
+                logger.LogHeading("IIS7 is not installed on this machine. IIS configuration setup terminated.");
+                return;
+            }
+
+            var envConfig = new WebConfiguration();
+
+            try
+            {
+                serverConfig
+                    .AddAppPool(envConfig.SiteName, "v4.0", ManagedPipelineMode.Integrated, ProcessModelIdentityType.LocalSystem)
+                    .WithProcessModel(envConfig.IdleTimeout, envConfig.PingingEnabled)
+                    .Commit();
+
+                serverConfig
+                    .AddSite(envConfig.SiteName, envConfig.PortNumber, envConfig.PortNumber)
+                    .AddApplication("/", envConfig.WebRoot, envConfig.SiteName)
+                    .WithLogging(false)
+                    .Commit();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e);
+            }
         }
     }
 }
---
+++ DGraph.Config/WebConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IisConfiguration.Configuration;
+
+namespace DGraph.Core.Configuration
+{
+    public class WebConfiguration : EnvironmentalConfig
+    {
+        public string SiteName
+        {
+            get
+            {
+                return "DGraph";
+            }
+        }
+
+        public int PortNumber
+        {
+            get
+            {
+                return 410;
+            }
+        }
+    }
+}
