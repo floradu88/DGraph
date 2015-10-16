86bd30c409d875bcb5b7f5d47c2a5d4319573357
raduflorescu <radu.florescu@totaljobsgroup.com>
2015-10-16 15:26:58 +0100

    added Search page and controller

---
+++ src/DGraph.Web/Controllers/SearchController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DGraph.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
