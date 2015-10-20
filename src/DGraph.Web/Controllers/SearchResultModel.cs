cf641782c087d91a91aa070353e0999647ab3cf4
raduflorescu <radu.florescu@totaljobsgroup.com>
2015-10-20 17:06:09 +0100

    added mising model

---
+++ src/DGraph.Web/Controllers/SearchResultModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using DGraph.Core.Domain;
+
+namespace DGraph.Web.Controllers
+{
+    public class SearchResultModel
+    {
+        public IList<Dependency> Results { get; set; }
+        public SearchViewModel Search { get; set; }
+
+        public SearchResultModel()
+        {
+            Results = new List<Dependency>();
+            Search = new SearchViewModel();
+        }
+    }
+}
